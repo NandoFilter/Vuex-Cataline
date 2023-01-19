aa34ffde53a124f7e7b8c990419156b4114081c3
Fernando Halmenschlager Filter <fernandohfilter10@gmail.com>
2023-01-19 20:17:26 -0300

    Alterado nome do repositório

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/Vuex-Challenge',
+  publicPath: '/Vuex-Cataline',
   transpileDependencies: true
 })
